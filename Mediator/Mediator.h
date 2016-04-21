9ca483cf59523b35beaf10e527a07554748c3e40
huangkexuan <huangkexuan@ucfgroup.com>
2016-04-21 17:27:22 +0800

    Initial Commit

---
+++ Mediator/Mediator.h
@@ -0,0 +1,13 @@
+//
+//  Mediator.h
+//  Mediator
+//
+//  Created by huangkexuan on 16/4/21.
+//  Copyright © 2016年 Cloud Funds Management. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Mediator : NSObject
+
+@end
---
+++ Mediator/Mediator.m
@@ -0,0 +1,13 @@
+//
+//  Mediator.m
+//  Mediator
+//
+//  Created by huangkexuan on 16/4/21.
+//  Copyright © 2016年 Cloud Funds Management. All rights reserved.
+//
+
+#import "Mediator.h"
+
+@implementation Mediator
+
+@end
